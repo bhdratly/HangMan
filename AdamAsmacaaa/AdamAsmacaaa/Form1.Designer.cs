4865a44e6c425b97fe23875a9d0e4fb39ef1a818
bhdratly <157316394+bhdratly@users.noreply.github.com>
2025-01-12 21:10:48 +0300

    HangMan
    Adam Asmaca, harf tahmin ederek gizli kelimeyi bulmaya çalıştığınız klasik bir kelime oyunudur. Yanıtlarınızı doğru tahmin ederek adamın asılmasını engellemeye çalışın.
    
    Teknolojiler:
    
    Dil: C#
    Platform: Visual Studio 2017

---
+++ AdamAsmacaaa/AdamAsmacaaa/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace AdamAsmacaaa
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblWordDisplay = new System.Windows.Forms.Label();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.tbGuess = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblWordDisplay
+            // 
+            this.lblWordDisplay.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblWordDisplay.Font = new System.Drawing.Font("Rockwell", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWordDisplay.Location = new System.Drawing.Point(1, 0);
+            this.lblWordDisplay.Name = "lblWordDisplay";
+            this.lblWordDisplay.Size = new System.Drawing.Size(460, 55);
+            this.lblWordDisplay.TabIndex = 0;
+            this.lblWordDisplay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Font = new System.Drawing.Font("Rockwell", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuess.Location = new System.Drawing.Point(1, 366);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(185, 81);
+            this.btnGuess.TabIndex = 1;
+            this.btnGuess.Text = "Guess";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // tbGuess
+            // 
+            this.tbGuess.Font = new System.Drawing.Font("Rockwell", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbGuess.Location = new System.Drawing.Point(310, 378);
+            this.tbGuess.Name = "tbGuess";
+            this.tbGuess.Size = new System.Drawing.Size(141, 58);
+            this.tbGuess.TabIndex = 2;
+            this.tbGuess.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(1, 73);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(450, 287);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(463, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.tbGuess);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.lblWordDisplay);
+            this.Name = "Form1";
+            this.Text = "AdamAsmaca";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblWordDisplay;
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.TextBox tbGuess;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ AdamAsmacaaa/AdamAsmacaaa/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdamAsmacaaa
+{
+    public partial class Form1 : Form
+    {
+        // Türkiye şehirlerinden oluşan kelime listesi
+        #region Veriable
+        List<string> words = new List<string> { "adana","adıyaman","afyonkarahisar","aksaray","amasya","ankara","antalya","ardahan","artvin","aydın","ağrı","balıkesir","bartın","batman","bayburt","bilecik","bingöl","bitlis","bolu","burdur","bursa","denizli","diyarbakır","düzce","edirne","elazığ","erzincan","erzurum","eskişehir","gaziantep","giresun","gümüşhane","hakkâri","hatay","ısparta","ığdır","kahramanmaraş","karabük","karaman","kars","kastamonu","kayseri","kilis","kocaeli","konya","kütahya","kırklareli","kırıkkale","kırşehir","malatya","manisa","mardin","mersin","muğla","muş","nevşehir","niğde","ordu","osmaniye","rize","sakarya","samsun","siirt","sinop","sivas","tekirdağ","tokat","trabzon","tunceli","uşak","van","yalova","yozgat","zonguldak",
+        "çanakkale","çankırı","çorum","istanbul","izmir","şanlıurfa","şırnak" };
+        int incorretGuess;
+        Random random;
+        String selectedWord;
+        char[] displayWord;
+        private int incorrectGuess;
+        #endregion
+        public Form1()
+        {
+            // Form bileşenlerini başlatır
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Yanlış tahmin sayısı için değişken
+            incorretGuess = 0;
+            // Rastgele kelime seçimi için kullanılan Random sınıfı
+            random = new Random();
+            // Seçilen kelime (tahmin edilmesi gereken kelime)
+            selectedWord = words[random.Next(words.Count)];
+            // Seçilen kelimenin uzunluğunda alt çizgi karakterlerinden oluşan bir dizi oluştur
+            displayWord = new string('_', selectedWord.Length).ToCharArray();
+            // Alt çizgileri boşluk ile ayırarak ekranda görüntüle
+            string formattedDisplayWord = string.Join(" ", displayWord);
+            lblWordDisplay.Text = formattedDisplayWord;
+
+        }
+        // "Tahmin Et" butonuna basıldığında çalışacak olay
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            // Kullanıcının girdiği harfi al ve küçük harfe çevi
+            char guess = tbGuess.Text.ToLower()[0];
+            // Doğru tahmin yapılıp yapılmadığını izlemek için bayrak
+            bool correctGuess = false;
+            // Seçilen kelimedeki harfleri kontrol et
+            for (int i = 0; i < selectedWord.Length; i++)
+            {
+                // Eğer tahmin edilen harf seçilen kelimedeki bir harfe eşitse
+
+                if (selectedWord[i] == guess)
+                {
+                    // Doğru tahmin edilen harfi alt çizginin yerine yerleştir
+                    displayWord[i] = guess;
+                    correctGuess = true;
+                }
+
+            }
+            // Güncellenmiş kelimeyi ekranda göster
+            lblWordDisplay.Text = string.Join(" ", displayWord);
+            if (!correctGuess)
+            {
+
+                // Adam asmaca görselini güncelle
+                UpdateAdamAsmacaImage();
+            }
+            // Eğer kelimenin tamamı tahmin edildiyse (alt çizgi kalmadıysa)                         
+            if (!lblWordDisplay.Text.Contains('_'))
+            {
+                MessageBox.Show("Tebrikler Kazandınız");
+                Application.Restart();
+            }
+
+        }
+        // Yanlış tahmin sayısına göre Adam Asmaca görselini güncelleyen metot
+        private void UpdateAdamAsmacaImage()
+        {
+            // Yanlış tahmin sayısını artır
+            incorrectGuess++;
+            // Yanlış tahmin sayısına göre farklı Adam Asmaca resimlerini göster
+            switch (incorrectGuess)
+            {
+                case 1:
+                    pictureBox1.Image = Properties.Resources._1;
+                    break;
+                case 2:
+                    pictureBox1.Image = Properties.Resources._2;
+                    break;
+                case 3:
+                    pictureBox1.Image = Properties.Resources._3;
+                    break;
+                case 4:
+                    pictureBox1.Image = Properties.Resources._4;
+                    break;
+                case 5:
+                    pictureBox1.Image = Properties.Resources._5;
+                    break;
+                case 6:
+                    pictureBox1.Image = Properties.Resources._6;
+                    break;
+                case 7:
+                    // 7. yanlış tahminde oyunun bittiğini belirt ve kaybedildiğini bildir
+                    pictureBox1.Image = Properties.Resources._7;
+                    MessageBox.Show("Kaybettiniz! Kelime Buydu " + selectedWord);
+                    // Oyunu baştan başlat
+                    Application.Restart();
+                    break;
+            }
+
+        }
+    }
+}
